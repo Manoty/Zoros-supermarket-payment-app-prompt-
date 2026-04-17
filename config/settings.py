d224a7c97e410ca1e3a08d1681fa936db3c2e979
Kevin Manoti <kevinnoty21@gmail.com>
2026-04-17 09:10:23 +0300

    Configure Django DB

--- config/settings.py
+++ config/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'supermarket_payments',
+        'USER': 'supermarket_user',
+        'PASSWORD': 'strongpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
